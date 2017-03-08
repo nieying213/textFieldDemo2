0e786bd7c5b4042b1519af1fb838dd1589c2c8c4
nieying03 <nieying03@baidu.com>
2017-03-08 18:02:49 +0800

    新增项目

---
+++ myDiaryTests/myDiaryTests.m
@@ -0,0 +1,39 @@
+//
+//  myDiaryTests.m
+//  myDiaryTests
+//
+//  Created by Nie,Ying(MTBD) on 2017/2/27.
+//  Copyright © 2017年 Nie,Ying(MTBD). All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface myDiaryTests : XCTestCase
+
+@end
+
+@implementation myDiaryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
